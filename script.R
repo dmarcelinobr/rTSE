0fd3d85ea7eb23bad93d51766b1b985f7d1c1c8a
Daniel Marcelino <dmarcelino@live.com>
2019-03-15 11:33:21 -0300

    script p/ baixar filiados

--- script.R
+++ script.R
@@ -1,6 +1,3 @@
-#Baixa e descompacta arquivos com filiados a partidos políticos
-#Truco - Agência Pública - setembro 2018
-
 #Baixa pacotes
 library(rvest)
 library(data.table)
@@ -8,13 +5,13 @@ library(plyr)
 library(rjson)
 
 #Lista estados
-uf <- c("ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mg", "ms", 
+uf <- c("ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mg", "ms",
         "mt", "pa", "pb", "pe", "pi", "pr", "rj", "rn", "ro","rr","rs","sc","se","sp","to")
 
 #Importa lista de partidos do TSE
   url_tse <- "http://filiaweb.tse.jus.br/filiaweb/portal/relacoesFiliados.xhtml"
 lista_partidos <- url_tse %>%
-  read_html("http://filiaweb.tse.jus.br/filiaweb/portal/relacoesFiliados.xhtml") %>% 
+  read_html("http://filiaweb.tse.jus.br/filiaweb/portal/relacoesFiliados.xhtml") %>%
   html_nodes(xpath = '//*[@id="partido"]//option') %>%
   html_attr("value")
 
@@ -24,7 +21,7 @@ for(partido in lista_partidos){
   arquivo <- paste0(partido,"_",estado,".zip")
   download.file(paste0("http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/filiados_",
                        partido,"_",estado,".zip"),arquivo, mode = "wb")}}
-    
+
 #Descompacta os arquivos do TSE
 arquivoszip <- list.files(pattern = "*.zip", full.names = TRUE)
 ldply(.data = arquivoszip, .fun = unzip)
